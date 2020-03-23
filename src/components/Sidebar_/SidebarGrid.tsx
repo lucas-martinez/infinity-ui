aa374d37cfae60a9e41ce9ce2810aba7f78a90d8
Lucas Martinez <lucas.martinez@microfocus.com>
2020-03-23 16:58:51 -0600

    Rename sidebar

--- src/components/Sidebar_/SidebarGrid.tsx
+++ src/components/Sidebar_/SidebarGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css, ThemeProps } from 'styled-components';
-import { Grid, GridProps } from '../../js/components//Grid';
+import { Grid, GridProps } from '../../js/components/Grid';
 import { ExtendedTheme } from '../../themes/cobalt';
 import pick from '../../themes/pick';
 
